1a28f10b7fdb40355298d00d5d19cf0761a1cd33
Gian Wong <giand2205@gmail.com>
2021-08-12 13:13:03 -0500

    edit import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gian', '12345', 1, 'Gian', 'Wong', 'gian@test.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'John', 'Doe', 'jhon.doe@test.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gian', '$2a$10$BJWK9h7i4HZ.DVwgSTkiuu7FkX7mV2R5Bi5h8Uwdp13NyzstSy96u', 1, 'Gian', 'Wong', 'gian@test.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$bmuoERDHt3uFH5V1hV2i2eN.mmcB4B9ainUxe6gjyUZyXMmxyEZsy', 1, 'John', 'Doe', 'jhon.doe@test.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
